8fb277363c215db64bf2b1bead7838fe0822af00
rostykusvasyl <rostykusvasyl@gmail.com>
2019-02-01 21:27:22 +0200

    Basic configuration of the provider.

---
+++ weatherapp/rp5/config.py
@@ -0,0 +1,15 @@
+RP5_PROVIDER_NAME = 'rp5'
+RP5_PROVIDER_TITLE = 'rp5.ua'
+
+# The name of the city and the address for which we will display the weather
+# conditions by default for site rp5.
+DEFAULT_RP5_LOCATION_NAME = 'Brody'
+DEFAULT_RP5_LOCATION_URL = ('http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B'
+                            '4%D0%B0_%D0%B2_%D0%91%D1%80%D0%BE%D0%B4%D0%B'
+                            '0%D1%85,_%D0%9B%D1%8C%D0%B2%D1%96%D0%B2%D1%81%'
+                            'D1%8C%D0%BA%D0%B0_%D0%BE%D0%B1%D0%BB%D0%B0%D1%'
+                            '81%D1%82%D1%8C')
+
+# Address for finding a place for which we will display weather conditions.
+RP5_BROWSE_LOCATIONS = ('http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_'
+                        '%D0%B2_%D1%81%D0%B2%D1%96%D1%82%D1%96')
